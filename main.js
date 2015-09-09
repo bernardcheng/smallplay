bcc6310e14164bc5047adb4f45c1161b21cc4676
Chung-Min Cheng <cchungmin@google.com>
2015-09-09 18:44:29 +0900

    Small scripts

---
+++ main.js
@@ -0,0 +1,15 @@
+$(function(){
+  var snsWrapper = document.getElementById('wrap-sns-pc');
+  var fixedSnsContent = document.getElementById('fixed-sns-pc');
+  var newPostsOffsetTop = $('#wrap-new-posts').offset().top;
+
+  $(window).scroll(function() {
+    if (snsWrapper && fixedSnsContent) {
+      if ($(window).scrollTop() && newPostsOffsetTop) {
+        $(fixedSnsContent).removeClass('fadein');
+      } else {
+        $(fixedSnsContent).addClass('fadein');
+      }
+    }
+  });
+})
