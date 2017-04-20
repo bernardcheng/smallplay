c4fe55606a7579ce3a902b6e5567de2399626f37
cchungmin <min427@gmail.com>
2017-04-20 19:50:54 +0900

    Adds dist build setting and minified/concated files.

---
+++ dist/smallplay.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sticky_1 = require("./sticky");
+console.log(sticky_1.run());
+
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function run() {
+    console.log('Start running sticky');
+}
+exports.run = run;
