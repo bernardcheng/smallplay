b94f843eb60738f67e21036f4456e605004bee14
Chung-Min Cheng <min427@gmail.com>
2017-05-09 19:18:26 +0900

    Adds progress bar.

--- dist/smallplay.js
+++ dist/smallplay.js
@@ -1,11 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var sticky_1 = require("./sticky");
-console.log(sticky_1.run());
+var progress_1 = require("./progress");
+console.log(sticky_1.runSticky());
+console.log(progress_1.runProgress());
 
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function run() {
-    console.log('Start running sticky');
+function runSticky() {
+    return 'Start running sticky';
 }
-exports.run = run;
+exports.runSticky = runSticky;
+exports.default = runSticky;
