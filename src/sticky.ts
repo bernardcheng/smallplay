4dcec4ab291afc078b111e71adf530984584ba16
Chung-Min Cheng <min427@gmail.com>
2017-06-07 17:58:50 +0900

    Adds fixes for sticky test.

--- src/sticky.ts
+++ src/sticky.ts
@@ -4,25 +4,34 @@ export class Sticky {
   private doc = document.documentElement;
   private body = document.body;
   private startEl: any;
+  private startOffset: any;
 
   constructor(optConfig?: any) {
     this.config = optConfig || {};
     this.targetEl = document.getElementById('sp-sticky-target');
     this.startEl = document.getElementById('sp-sticky-start');
 
-    this.init();
+    if (this.targetEl && this.startEl) {
+      this.init();
+    }
   }
 
   private init() {
+    this.startOffset = this.startEl.getBoundingClientRect();
+
     window.addEventListener('scroll', this.stickyMonitor);
   }
 
   private stickyMonitor = () => {
-    let startOffset: any = this.startEl.getBoundingClientRect();
     let docHeight: number =
-        (this.doc && this.doc.scrollTop || this.body && this.body.scrollTop || 0);
+        (this.doc && this.doc.scrollTop || this.body && this.body.scrollTop ||
+             0);
 
-    this.targetEl.classList.toggle('active', docHeight > startOffset.top);
+    this.targetEl.classList.toggle('active', docHeight > this.startOffset.top);
+  };
+
+  result() {
+    return 'Sticky';
   };
 }
 
