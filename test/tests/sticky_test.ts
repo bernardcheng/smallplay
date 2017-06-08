7000ae524693df1d7226dc8bb1d273bd8a2573aa
Chung-Min Cheng <min427@gmail.com>
2017-06-08 20:12:07 +0900

    Adds setting for using Karma & PhantomJS.

--- test/tests/sticky_test.ts
+++ test/tests/sticky_test.ts
@@ -1,40 +1,23 @@
 import { Sticky } from '../../src/sticky';
 import { expect } from 'chai';
 // if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
-import * as jsdom from 'jsdom';
 import 'mocha';
 
 describe('A sticky behavior', () => {
-  const HTML =
-      '<head>' +
-      '  <link rel="stylesheet" href="../browser/stylesheets/common.css">' +
-      '</head>' +
-      '<body>' +
-      '<header id="sp-sticky-target" class="sticky-header-test">' +
-      '  Sticky Header' +
-      '</header>' +
-      '<div id="sp-sticky-start" class="sticky-start-test">' +
-      '  After me, then show' +
-      '</div>' +
-      '</body>';
-  const { JSDOM } = jsdom;
-  const dom = new JSDOM(HTML, {
-    contentType: "text/html",
-    resources: 'usable',
-    includeNodeLocations: true
+  let stickyInstance, headerEl, startEl;
+
+  before(() => {
+    // document.body.innerHTML = window.__html__['test/browser/index.html'];
   });
 
-  let window, document, stickyInstance, headerEl, startEl;
-
   beforeEach(() => {
-    window = dom.window;
-    document = dom.window.document;
     headerEl = document.getElementById('sp-sticky-target');
     startEl = document.getElementById('sp-sticky-start');
     stickyInstance = new Sticky();
   });
 
   it('should return hello world', () => {
+    window.scrollTo(0, 500);
     expect(stickyInstance.result()).to.equal('Sticky');
   });
 
@@ -42,4 +25,3 @@ describe('A sticky behavior', () => {
 
   });
 });
-
