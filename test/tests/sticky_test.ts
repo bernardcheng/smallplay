ca3ff2ecb7d90db5d550a3b155580b520450f61c
Chung-Min Cheng <min427@gmail.com>
2017-06-09 16:53:35 +0900

    Adds tests for sticky.

--- test/tests/sticky_test.ts
+++ test/tests/sticky_test.ts
@@ -1,27 +1,50 @@
+/// <reference path="declaration.ts" />
+
+'use strict';
+
 import { Sticky } from '../../src/sticky';
 import { expect } from 'chai';
 // if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
 import 'mocha';
 
+
 describe('A sticky behavior', () => {
   let stickyInstance, headerEl, startEl;
-
-  before(() => {
-    // document.body.innerHTML = window.__html__['test/browser/index.html'];
-  });
+  addDefaultCSS();
 
   beforeEach(() => {
+    document.body.innerHTML = __html__['test/browser/index.html'];
     headerEl = document.getElementById('sp-sticky-target');
     startEl = document.getElementById('sp-sticky-start');
     stickyInstance = new Sticky();
   });
 
-  it('should return hello world', () => {
-    window.scrollTo(0, 500);
-    expect(stickyInstance.result()).to.equal('Sticky');
+  it('should appear while scrolling y-axis to 700', () => {
+    window.scrollTo(0, 700);
+
+    expect(headerEl.classList.contains('active')).to.be.true;
+  });
+
+  it('should not appear while scrolling y-axis to 100', () => {
+    window.scrollTo(0, 100);
+
+    expect(headerEl.classList.contains('active')).to.be.false;
   });
 
   afterEach(() => {
-
+    let bodeNode = document.body;
+    while (bodeNode.firstChild) {
+       bodeNode.removeChild(bodeNode.firstChild);
+    }
   });
+
+  function addDefaultCSS() {
+    let head  = document.getElementsByTagName('head')[0];
+    let link  = document.createElement('link');
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'base/test/browser/stylesheets/common.css';
+    link.media = 'all';
+    head.appendChild(link);
+  }
 });
